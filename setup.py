2598185a5c0cd0bd78721ee6113445a30ed7886f
Ben Cole <benc@netsight.co.uk>
2015-03-19 13:15:40 +0000

    Fixed - Make it installable

--- setup.py
+++ setup.py
@@ -5,21 +5,26 @@ Part of PyZFSCore.
 Copyright 2013 Trevor Joynson
 
 """
+from setuptools import setup, Extension
 
-from setuptools import setup
-
-import pyzfscore
-import pyzfscore.libzfs
-import pyzfscore.libnvpair
+libzfs_ext = Extension(
+    name='libzfs',
+    define_macros=[('NDEBUG', 1), ('HAVE_IOCTL_IN_SYS_IOCTL_H', 1)],
+    libraries=['nvpair', 'zfs', 'zpool'],
+    sources=['pyzfscore/__pycache__/_cffi__x8618e7c5xe2b6f166.c'],
+    include_dirs=['/usr/include/libzfs', '/usr/include/libspl']
+)
 
 setup(
     name='pyzfscore',
     version='0.0.1',
     description='ZFS CFFI wrapper.',
     license='GPL3',
-    maintainer='Trevor Joynson',
-    maintainer_email='github@skywww.net',
-    url='http://github.com/akatrevorjay/pyzfscore',
+    author='Trevor Joynson',
+    author_email='github@skywww.net',
+    maintainer='Ben Cole',
+    maintainer_email='wengole@gmail.com',
+    url='http://github.com/wengole/pyzfscore',
     packages=[
         'pyzfscore',
         'pyzfscore.libzfs',
@@ -29,7 +34,6 @@ setup(
     install_requires=['cffi'],
     zip_safe=False,
     ext_modules=[
-        pyzfscore.libzfs.ffi.verifier.get_extension(),
-        pyzfscore.libnvpair.ffi.verifier.get_extension(),
+        libzfs_ext
     ],
 )
