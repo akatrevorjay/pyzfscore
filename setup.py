ebc53e838ba20e950c803c0017d9e201e525296c
Trevor Joynson <github@skywww.net>
2015-04-02 17:16:16 -0400

    Revert "Added - cffi as an install requirement"

--- setup.py
+++ setup.py
@@ -5,35 +5,30 @@ Part of PyZFSCore.
 Copyright 2013 Trevor Joynson
 
 """
-from setuptools import setup, Extension
 
-libzfs_ext = Extension(
-    name='libzfs',
-    define_macros=[('NDEBUG', 1), ('HAVE_IOCTL_IN_SYS_IOCTL_H', 1)],
-    libraries=['nvpair', 'zfs', 'zpool'],
-    sources=['pyzfscore/__pycache__/_cffi__x8618e7c5xe2b6f166.c'],
-    include_dirs=['/usr/include/libzfs', '/usr/include/libspl']
-)
+from setuptools import setup
+
+import pyzfscore
+import pyzfscore.libzfs
+import pyzfscore.libnvpair
 
 setup(
     name='pyzfscore',
     version='0.0.1',
     description='ZFS CFFI wrapper.',
     license='GPL3',
-    author='Trevor Joynson',
-    author_email='github@skywww.net',
-    maintainer='Ben Cole',
-    maintainer_email='wengole@gmail.com',
-    url='http://github.com/wengole/pyzfscore',
-    packages=[
-        'pyzfscore',
-        'pyzfscore.libzfs',
-        'pyzfscore.flufl',
-        'pyzfscore.flufl.enum',
-    ],
-    install_requires=['cffi'],
+    maintainer='Trevor Joynson',
+    maintainer_email='github@skywww.net',
+    url='http://github.com/akatrevorjay/pyzfscore',
+    packages=['pyzfscore',
+              'pyzfscore.libzfs',
+              'pyzfscore.flufl',
+              'pyzfscore.flufl.enum',
+              ],
+    #use_2to3=True,
+
     zip_safe=False,
-    ext_modules=[
-        libzfs_ext
-    ],
+    ext_modules=[pyzfscore.libzfs.ffi.verifier.get_extension(),
+                 pyzfscore.libnvpair.ffi.verifier.get_extension(),
+                 ],
 )
